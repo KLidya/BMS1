41411cd2601d46f82ba58bbb6efcfcb8e8e35d71
Masum710 <patelmasum710@gmail.com>
2024-04-07 21:57:05 -0500

    My Bank Management System Project Java files

--- src/main/java/com/itexps/bms1/IndividualCustomer.java
+++ src/main/java/com/itexps/bms1/IndividualCustomer.java
@@ -4,6 +4,8 @@
  */
 package com.itexps.bms1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author patel
@@ -12,4 +14,49 @@ public class IndividualCustomer extends Customer{
     private String socialSecurityNumber;
     private String occupation;
     private double incomeLevel;
+
+    public IndividualCustomer(String socialSecurityNumber, String occupation, double incomeLevel, int id, String name, String email, String address, String phone, CustomerType custType) {
+        super(id, name, email, address, phone, custType);
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.occupation = occupation;
+        this.incomeLevel = incomeLevel;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(String socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public double getIncomeLevel() {
+        return incomeLevel;
+    }
+
+    public void setIncomeLevel(double incomeLevel) {
+        this.incomeLevel = incomeLevel;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(ArrayList<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "IndividualCustomer{" + "socialSecurityNumber=" + socialSecurityNumber + ", occupation=" + occupation + ", incomeLevel=" + incomeLevel + '}';
+    }
+    
 }
